0de46ef3728b971693d3980901bdfa0554514f32
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-03 11:37:49 -0500

    created weather card

--- src/components/SearchMain.jsx
+++ src/components/SearchMain.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "../style.css";
+import WeatherDetails from "./WeatherDetails";
 
 function SearchMain() {
   const [searchTerm, setSearchTerm] = useState("");
   console.log(searchTerm);
   return (
-    <div className='wrap'>
-      <div className='search'>
-        <input
-          type='search'
-          placeholder='City Name'
-          id='search'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+    <>
+      <div className='wrap'>
+        <div className='search'>
+          <input
+            type='search'
+            placeholder='City Name'
+            id='search'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <button className='searchButton'>Search</button>
       </div>
-      <button className='searchButton'>Search</button>
-    </div>
+      <WeatherDetails />
+    </>
   );
 }
 
---
+++ src/components/WeatherDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function WeatherDetails() {
+  return (
+    <>
+      <article className='widget'>
+        <div className='weatherIcon'>
+          <i className='wi wi-day-sunny'></i>
+        </div>
+      </article>
+    </>
+  );
+}
+
+export default WeatherDetails;
